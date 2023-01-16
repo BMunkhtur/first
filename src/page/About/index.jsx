a400d5873ece711af781a167ec70e25743636f96
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-01-16 14:45:10 +0800

    up

--- src/page/About/index.jsx
+++ src/page/About/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import style from "./style.css";
 
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 const About = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   return (
     <div className="container">
       <section class="home" id="home">
@@ -19,7 +32,27 @@ const About = () => {
             <span>About Me</span>
             <i class="fas fa-arrow-circle-down"></i>
           </a>
-          <div class="socials"></div>
+          <div class="socials">
+            <ul class="social-icons">
+              <li>
+                <SocialIcon network="twitter" />
+              </li>
+              <li>
+                {" "}
+                <SocialIcon network="google" />
+              </li>
+              <li>
+                <SocialIcon network="instagram" />
+              </li>
+              <li>
+                {" "}
+                <SocialIcon network="youtube" />
+              </li>
+              <li>
+                <SocialIcon network="facebook" />
+              </li>
+            </ul>
+          </div>
         </div>
         <div class="image">
           <img draggable="false" class="tilt" src="./pic/bi.png" alt="" />
